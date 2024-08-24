c57640e94d37c97821e5e0726883ea7b92c1d63a
rehan-ankalgi-7t2 <rehanankalgi7t2@gmail.com>
2024-08-24 14:51:31 +0530

    django forms intro

--- polls/chai/urls.py
+++ polls/chai/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "chai"
 urlpatterns = [
     path("", views.all_chai, name="all_chai"), # /chai/
-    path("<int:chai_id>/", views.chai_detail, name='chai_details')
+    path("<int:chai_id>/", views.chai_detail, name='chai_details'),
+    path("chai_stores/", views.chai_store_view, name='chai_stores')
 ]
--- polls/chai/views.py
+++ polls/chai/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from .models import ChaiVariety
+from .forms import ChaiVareityform
+from .models import ChaiVariety, Store
 
 # Create your views here.
 def all_chai(request):
@@ -9,3 +10,16 @@ def all_chai(request):
 def chai_detail(request, chai_id):
     chai = get_object_or_404(ChaiVariety, pk=chai_id)
     return render(request, 'chai/chai_detail.html', {'chai': chai})
+
+def chai_store_view(request):
+    stores = None
+    if request.method == 'POST':
+        # form with values filled submission state
+        form = ChaiVareityform(request.POST)
+        if form.is_valid():
+            chai_variety = form.cleaned_data['chai_variety'] #from the Chai Variety Form
+            stores = Store.objects.filter(chai_varieties = chai_variety)
+    else:
+        '''form default state without any value association, not handling this case will result in UnboundLocalError: cannot access local variable 'form' where it is not associated with a value'''
+        form = ChaiVareityform()
+    return render(request, 'chai/chai_store.html', {'stores': stores, 'form': form})
